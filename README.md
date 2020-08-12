cd7402ae0597c14a4d46254b409eacc51f895252
jmjhox <josemiguelaparicio507@gmail.com>
2020-08-12 14:56:06 -0500

    modified portfolio and fixed some bugs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # jmjhox.github.io
 para echar un vistazo a mi portafolio:
 https://jmjhox.github.io/
+
+##Este portafolio fue hecho a partir de una modificación de una template gratuita, créditos a su autor.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Spatial by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
 
 (function($) {
 
@@ -71,3 +66,57 @@
 	});
 
 })(jQuery);
+
+//Code for carrusel slider
+var current = 0;
+var imagenes = new Array();
+ 
+$(document).ready(function() 
+{
+    var numImages = 9;
+	if (numImages <= 3) 
+		{
+        $('.right-arrow').css('display', 'none');
+        $('.left-arrow').css('display', 'none');
+    	}
+ 
+	$('.left-arrow').on('click',function() 
+	{
+		if (current > 0) 
+			{
+				current = current - 1;
+			} 
+		else 
+			{
+				current = numImages - 3;
+			}
+ 
+        $(".carrusel").animate({"left": -($('#product_'+current).position().left)}, 600);
+ 
+        return false;
+    });
+
+    $('.left-arrow').on('hover', function() {
+        $(this).css('opacity','0.5');
+    }, function() {
+        $(this).css('opacity','1');
+    });
+ 
+    $('.right-arrow').on('hover', function() {
+        $(this).css('opacity','0.5');
+    }, function() {
+        $(this).css('opacity','1');
+    });
+ 
+    $('.right-arrow').on('click', function() {
+        if (numImages > current + 3) {
+            current = current+1;
+        } else {
+            current = 0;
+        }
+ 
+        $(".carrusel").animate({"left": -($('#product_'+current).position().left)}, 600);
+ 
+        return false;
+    }); 
+ });
